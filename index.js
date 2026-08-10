605c96f12927c5f2685ec8e3892bbaa707e090ce
al7mol38-code <al7mol38@gmail.com>
2026-08-10 13:15:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 require("dotenv").config();
 
+const express = require("express");
 const { Client, GatewayIntentBits } = require("discord.js");
 const moment = require("moment-timezone");
 
+const app = express();
+app.get("/", (req, res) => res.send("Bot is running!"));
+app.listen(process.env.PORT || 3000);
+
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [GatewayIntentBits.Guilds],
 });
 
-const CHANNEL_ID =  process.env.CHANNEL_ID;
+const CHANNEL_ID = process.env.CHANNEL_ID;
 
 function createMessage() {
-  return `# 🌍 Server Times
+  return `# 🌎 Server Times
 
 🇸🇦 Saudi Arabia: ${moment().tz("Asia/Riyadh").format("HH:mm")}
 🇯🇵 Tokyo: ${moment().tz("Asia/Tokyo").format("HH:mm")}
@@ -20,7 +25,7 @@ function createMessage() {
 🇧🇷 São Paulo: ${moment().tz("America/Sao_Paulo").format("HH:mm")}`;
 }
 
-client.once("ready", async () => {
+client.once("clientReady", async () => {
   console.log(`${client.user.tag} جاهز`);
 
   const channel = await client.channels.fetch(CHANNEL_ID);
@@ -29,17 +34,7 @@ client.once("ready", async () => {
 
   setInterval(async () => {
     await channel.send(createMessage());
-  }, 60 * 60 * 1000);
+  }, 60 * 1000);
 });
 
 client.login(process.env.TOKEN);
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Bot is running!");
-});
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Web server started");
-});
