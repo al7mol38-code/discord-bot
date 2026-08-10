27b90b1f6b5641014f57461f2662e0cd89e3d9f3
al7mol38-code <al7mol38@gmail.com>
2026-08-10 12:11:25 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+
+const { Client, GatewayIntentBits } = require("discord.js");
+const moment = require("moment-timezone");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+const CHANNEL_ID = "ضع_ايدي_الروم_هنا";
+
+function createMessage() {
+  return `# 🌍 Server Times
+
+🇸🇦 Saudi Arabia: ${moment().tz("Asia/Riyadh").format("HH:mm")}
+🇯🇵 Tokyo: ${moment().tz("Asia/Tokyo").format("HH:mm")}
+🇸🇬 Singapore: ${moment().tz("Asia/Singapore").format("HH:mm")}
+🇦🇺 Sydney: ${moment().tz("Australia/Sydney").format("HH:mm")}
+🇵🇱 Warsaw: ${moment().tz("Europe/Warsaw").format("HH:mm")}
+🇧🇷 São Paulo: ${moment().tz("America/Sao_Paulo").format("HH:mm")}`;
+}
+
+client.once("ready", async () => {
+  console.log(`${client.user.tag} جاهز`);
+
+  const channel = await client.channels.fetch(CHANNEL_ID);
+
+  await channel.send(createMessage());
+
+  setInterval(async () => {
+    await channel.send(createMessage());
+  }, 60 * 60 * 1000);
+});
+
+client.login(process.env.TOKEN);
