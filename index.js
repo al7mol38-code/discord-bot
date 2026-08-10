2543fb7ca89b252545035e483e3bc74159856c30
al7mol38-code <al7mol38@gmail.com>
2026-08-10 13:06:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -33,3 +33,13 @@ client.once("ready", async () => {
 });
 
 client.login(process.env.TOKEN);
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is running!");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Web server started");
+});
